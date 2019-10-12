74f3550766612613c215cc700e7552248fe5e400
rizveeerprojects <rarndc@gmail.com>
2019-10-12 21:27:13 +0600

    formation of folders

---
+++ Algorithm Templates/Data-structure/fenwick_tree.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//C++ implementation of fenwick tree/binary index tree  
+
+struct FenwickTree{
+	
+	//query from fenwick tree 
+	void query(){
+		
+	}
+	//update of fenwicktree
+	void update(){
+		
+	}
+	
+};
+
+int main(void){
+	
+}
