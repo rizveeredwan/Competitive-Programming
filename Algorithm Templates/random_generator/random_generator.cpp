67556cde0b4b15b2c782c5789276e58848555753
rizveeerprojects <rarndc@gmail.com>
2022-03-07 18:04:35 +0600

    random generator

--- Algorithm Templates/random_generator/random_generator.cpp
+++ Algorithm Templates/random_generator/random_generator.cpp
@@ -22,6 +22,33 @@ void string_generator(int n){
     cout<<s<<endl;
 }
 
+vector<int> input_generation(int n, int min_value, int max_value) {
+    srand(time(0));
+    map<int,bool>M;
+    vector<int>V;
+    int value;
+    auto it = M.begin();
+    while (V.size() < n) {
+        value = rand()%max_value+min_value;
+        V.push_back(value);
+        /*if (M.find(value) == M.end()) {
+            M[value]=true;
+            V.push_back(value);
+        }*/
+    }
+    return V;
+}
+
+template<typename T> void print_vector(vector<T>V){
+    for(int i=0; i<V.size(); i++){
+        cout<<V[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
 int main(void){
-    string_generator(12);
+    //string_generator(12);
+    vector<int> V = input_generation(10, 1, 70);
+    print_vector(V);
 }
