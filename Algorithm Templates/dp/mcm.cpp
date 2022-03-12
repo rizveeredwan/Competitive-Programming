2cfd3a52d8ea3a7a17db389f77bdcf71c1d4cbb6
rizveeerprojects <rarndc@gmail.com>
2022-03-13 01:54:52 +0600

    added iterative mcm

--- Algorithm Templates/dp/mcm.cpp
+++ Algorithm Templates/dp/mcm.cpp
@@ -21,6 +21,28 @@ int mcm(vector<pair<int,int>>V, int i, int k, int dp[MAX+1][MAX+1]){
     return res;
 }
 
+void iterative_mcm(vector<pair<int,int>>V, int dp[MAX+1][MAX+1]){
+    int n = V.size();
+    int k, j;
+    int res;
+    for(int i = 0; i< n; i++)
+        dp[i][i] = 0;
+    for (int length = 2; length<=n; length++)
+    {
+        for(int i = 0; i<=n-2; i++)
+        {
+            k = i+length-1;
+            res = MAX_INF;
+            for(int mid=1; mid<length; mid++)
+            {
+                j = i+mid-1;
+                res = min(res, dp[i][j] + dp[j+1][k] + V[i].first * V[j].second * V[k].second);
+            }
+            dp[i][k] = res;
+        }
+    }
+}
+
 vector<pair<int, int>> input(){
     int n;
     cin>>n;
@@ -34,12 +56,15 @@ vector<pair<int, int>> input(){
 }
 
 int main(void){
-    freopen("in1.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
     vector<pair<int, int>>V = input();
     int dp[MAX+1][MAX+1];
     memset(dp, -1, sizeof(dp));
     cout<<dp[0][2] << dp[0][5]<<endl;
     int res = mcm(V, 0, V.size()-1, dp);
     cout<<res<<endl;
+    // iterative DP
+    iterative_mcm(V, dp);
+    cout<<dp[0][V.size()-1]<<endl;
     return 0;
 }
