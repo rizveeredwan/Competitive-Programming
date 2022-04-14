e90a4cff29f8fdc13940855765957e829e0a1561
rizveeerprojects <rarndc@gmail.com>
2022-04-15 00:17:50 +0600

    added litcode folder

---
+++ leetcode/410.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000
+
+
+int input(int arr[]){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    return n;
+}
+
+void solution(int n, int arr[]){
+}
+
+int main(void){
+    int arr[MAX+1];
+    int n = input(arr);
+
+}
